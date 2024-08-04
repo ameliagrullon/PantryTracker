501b9d631eff6da458cb52ef2c6a16f9f635f862
Amelia G <ameliaginquiries@gmail.com>
2024-08-04 18:42:16 -0400

    next

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-'use client';
-import Image from "next/image";
-import { useState, useEffect } from "react";
-import { Box, Modal, Stack, TextField, Typography, Button, Pagination, Snackbar, Alert, CircularProgress } from "@mui/material";
-import { firestore } from "@/firebase";
-import { collection, deleteDoc, doc, getDocs, query, setDoc, getDoc } from "firebase/firestore";
-import { getFirestore } from 'firebase/firestore';
-import { initializeApp } from 'firebase/app';
+'use client'
+import Image from "next/image"
+import { useState, useEffect } from "react"
+import { Box, Modal, Stack, TextField, Typography, Button, Pagination, Snackbar, Alert, CircularProgress } from "@mui/material"
+import { firestore } from "@/firebase"
+import { collection, deleteDoc, doc, getDocs, query, setDoc, getDoc } from "firebase/firestore"
 
 const style = {
   position: 'absolute',
